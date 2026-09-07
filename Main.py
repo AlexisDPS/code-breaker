1864c54e232605fbd8dd027361e57494669bcc00
AlexisDPS <depaza612@gmail.com>
2026-09-07 16:09:33 -0700

    Added imports, required functions, and main function

---
+++ Main.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+import random
+import check_input
+
+
+def generate_code():
+    pass
+
+
+def get_guess():
+    pass
+
+
+
+def check_guess(code, guess):
+    pass
+
+
+def display_results(results):
+    pass
+
+
+
+def main():
+    pass
+
+main()
